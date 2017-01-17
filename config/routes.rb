8a7c0898b881cf5456372f96f209850c7cbd3b02
dongmy54 <615760433@qq.com>
2017-01-17 10:57:29 +0800

    please check code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
+#Rails.application.routes.draw do
+ # devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   #  namespace :admin do
+   #   resources :jobs do
+
+    #  member do
+      #  post :publish
+      #  post :hide
+      #end
+    #end
+#     resources :jobs
+# end
+      #resources :jobs
+     #root 'jobs#index'
+
+#end
+#end
+
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-     namespace :admin do
-    resources :jobs
-end
-      resources :jobs
-   root 'jobs#index'
+
+   resources :jobs
+   root 'welcome#index'
 end
